c7c2cc58a8d4157a329662ac56c593b7990f56df
WilliamEklundITHS <william.eklund@iths.se>
2024-11-19 13:48:36 +0100

    new email recipent

--- studentbostad_scraper/studentbostad_scraper/spiders/spider.py
+++ studentbostad_scraper/studentbostad_scraper/spiders/spider.py
@@ -61,7 +61,7 @@ class MySpider(scrapy.Spider):
                     },
                     "To": [
                         {
-                            "Email": "w05446795@gmail.com",  # Replace with the recipient's email
+                            "Email": "pederburrstock@gmail.com",  # Replace with the recipient's email
                             "Name": "Recipient Name"
                         }
                     ],
